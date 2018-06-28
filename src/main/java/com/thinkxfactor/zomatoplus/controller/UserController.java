b61af9db9d89828301089352a62a05519cf4df39
anirbanc4u <40594012+anirbanc4u@users.noreply.github.com>
2018-06-28 10:14:02 +0530

    Update UserController.java

--- src/main/java/com/thinkxfactor/zomatoplus/controller/UserController.java
+++ src/main/java/com/thinkxfactor/zomatoplus/controller/UserController.java
@@ -34,28 +34,9 @@ public class UserController {
 		List<User> listofusers=userRepository.findAll();
 		return listofusers;
 	}
-
-//	@GetMapping("/loginEn")
-//	public String userLogin(@RequestParam("username") String username,
-//			@RequestParam("password") String password)
-//	{
-//		User obj= new User();
-//		obj.setUsername("ASD");
-//		obj.setPassword("123");
-//		return "You are logged in";
-//	}
 	
-	
-//	@PostMapping("/create")
-//	public User usercreate(@RequestBody User us) {
-//		User u1=new User("A","1","abc@gmail.com","9876543201");
-//		System.out.println(u1.toString());
-//		return us;
-//		
-//	}
-//	
 	@PostMapping("/login")
-	public User userlogin(@RequestBody String name, String password)
+	public User userlogin(@RequestParam("name") String name, @RequestParam("password") String password)
 	{	
 		User persistedUser=userRepository.findByNameAndPassword(name,password);
 		return persistedUser;
